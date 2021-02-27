f08b496d7a03ed5cdce38b4bff2b5abc36752148
mayankgarg0987 <55484263+mayankgarg0987@users.noreply.github.com>
2021-02-27 18:33:13 +0530

    Add files via upload

---
+++ SCRIPTS/isa-all.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+import time
+
+driver=webdriver.Chrome()
+
+# opening the target website
+driver.get('https://cuchd.blackboard.com')
+
+# entering username and password
+searchbox=driver.find_element_by_xpath('//*[@id="user_id"]')
+searchbox.send_keys('19BCS3516')
+searchbox=driver.find_element_by_xpath('//*[@id="password"]')
+searchbox.send_keys('Incorrect99(')
+
+# time.sleep(1)
+# 2 seconds delay
+cookieButton=driver.find_element_by_xpath('//*[@id="agree_button"]')
+cookieButton.click()
+
+# clicking on sign-in button
+searchButton=driver.find_element_by_xpath('//*[@id="entry-login"]')
+searchButton.click()
+
+time.sleep(10)
+# clicking on the link to enter in the class
+classLink=driver.find_element_by_xpath('//*[@id="course-link-_19144_1"]/h4').click()
+
+time.sleep(8)
+# clicking on 'join session'
+joinSession=driver.find_element_by_xpath('//*[@id="sessions-list-dropdown"]/span').click()
+
+time.sleep(1)
+# clicking on 'course room'
+courseRoom=driver.find_element_by_xpath('//*[@id="sessions-list"]/li/a/span').click()
